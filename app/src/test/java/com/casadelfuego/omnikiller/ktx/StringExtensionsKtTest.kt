61b7d95aae85d885fde9b81b85b890f91cb0ec3e
Douglas Kazumi - Vendor <DKazumi@walmartlabs.com>
2018-06-02 03:41:17 -0700

    Tests for extensions

--- app/src/test/java/com/casadelfuego/omnikiller/ktx/StringExtensionsKtTest.kt
+++ app/src/test/java/com/casadelfuego/omnikiller/ktx/StringExtensionsKtTest.kt
@@ -1,24 +1,77 @@
 package com.casadelfuego.omnikiller.ktx
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
+import java.util.*
 
 class StringExtensionsKtTest {
 
     @Test
-    fun toBoolean() {
-    }
+    fun string0ToBoolean() = assertFalse("0".toBoolean())
 
     @Test
-    fun isFalse() {
-    }
+    fun stringEmptyToBoolean() = assertFalse("".toBoolean())
+
+    @Test
+    fun stringFalseToBoolean() = assertFalse("false".toBoolean())
+
+    @Test
+    fun stringFaLsEToBoolean() = assertFalse("FaLsE".toBoolean())
+
+    @Test
+    fun stringSpaceToBoolean() = assertFalse(" ".toBoolean())
+
+    @Test
+    fun stringRandomToBoolean() = assertTrue("random".toBoolean())
+
+    @Test
+    fun string1ToBoolean() = assertTrue("1".toBoolean())
+
+    @Test
+    fun string0IsFalse() = assertTrue("0".isFalse())
+
+    @Test
+    fun stringEmptyIsFalse() = assertTrue("".isFalse())
+
+    @Test
+    fun stringFalseIsFalse() = assertTrue("false".isFalse())
+
+    @Test
+    fun stringFaLsEIsFalse() = assertTrue("FaLsE".isFalse())
+
+    @Test
+    fun stringSpaceIsFalse() = assertTrue(" ".isFalse())
+
+    @Test
+    fun stringRandomIsFalse() = assertFalse("random".isFalse())
+
+    @Test
+    fun string1IsFalse() = assertFalse("1".isFalse())
 
     @Test
     fun toDate() {
+        val date = "2019-05-31".toDate()
+
+        val calendar = Calendar.getInstance()
+        calendar.time = date
+
+        assertEquals(2019, calendar.get(Calendar.YEAR))
+        assertEquals(5, calendar.get(Calendar.MONTH) + 1)
+        assertEquals(31, calendar.get(Calendar.DAY_OF_MONTH))
     }
 
     @Test
     fun toDateTime() {
+        val date = "2018-05-24 20:34:48".toDateTime()
+
+        val calendar = Calendar.getInstance()
+        calendar.time = date
+
+        assertEquals(2018, calendar.get(Calendar.YEAR))
+        assertEquals(5, calendar.get(Calendar.MONTH) + 1)
+        assertEquals(24, calendar.get(Calendar.DAY_OF_MONTH))
+        assertEquals(20, calendar.get(Calendar.HOUR_OF_DAY))
+        assertEquals(34, calendar.get(Calendar.MINUTE))
+        assertEquals(48, calendar.get(Calendar.SECOND))
     }
 }
