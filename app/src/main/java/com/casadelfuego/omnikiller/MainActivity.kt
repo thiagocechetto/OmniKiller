623d927a8fac72e7f86466a67c93331ffb1528d9
Thiago Cechetto <thiagocechetto@Cechettos-MacBook-Pro.local>
2018-06-02 00:41:30 -0700

    Added android studio project

---
+++ app/src/main/java/com/casadelfuego/omnikiller/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.casadelfuego.omnikiller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.casadelfuego.omnikiller.ui.main.MainFragment
+
+class MainActivity: AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main_activity)
+    if (savedInstanceState == null) {
+      supportFragmentManager.beginTransaction()
+          .replace(R.id.container, MainFragment.newInstance())
+          .commitNow()
+    }
+  }
+
+}
